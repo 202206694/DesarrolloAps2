287b0eb60fd307226dcb699a1097fe789301ed69
202206694 <112829086+202206694@users.noreply.github.com>
2025-05-16 01:41:17 +0200

    render

--- subasta_das/the_muffins/pages/registro.jsx
+++ subasta_das/the_muffins/pages/registro.jsx
@@ -87,7 +87,7 @@ const Registro = () => {
         };
     
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/users/register/', {
+            const response = await fetch('https://backend-the-muffins.onrender.com/api/users/register/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(userData)
